7cebe1029008c992cd8b5c9f2379ceb156ff6631
chondosha1234 <jmiller61193@gmail.com>
2023-08-07 20:08:42 -0400

    wrote basic board class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,39 @@
+
+
 public class Board {
+
+    final int ROW_COUNT, COL_COUNT;
+    private Cell[][] cells;
+
+    public Board(int rowCount, int columnCount) {
+        ROW_COUNT = rowCount;
+        COL_COUNT = columnCount;
+
+        cells = new Cell[ROW_COUNT][COL_COUNT];
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COL_COUNT; col++) {
+                cells[row][col] = new Cell(row, col);
+            }
+        }
+    }
+
+    public Cell[][] getBoard() { return this.cells; }
+
+    public void setCells(Cell[][] cells) {
+        this.cells = cells;
+    }
+
+    public void generateFood() {
+        int row = 0;
+        int col = 0;
+
+        // find an empty cell and break from loop
+        while(true) {
+            row = (int) (Math.random() * ROW_COUNT);
+            col = (int) (Math.random() * COL_COUNT);
+            if (cells[row][col].getCellType() != CellType.SNAKE_SEGMENT)
+                break;
+        }
+        cells[row][col].setCellType(CellType.FOOD);
+    }
 }
