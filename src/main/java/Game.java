2fc261df9c66274e5167eb66476f3ee99d17d951
chondosha1234 <jmiller61193@gmail.com>
2023-08-08 20:58:02 -0400

    write framework of game class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,75 @@
+
 public class Game {
+
+    public static final int DIRECTION_NONE = 0;
+    public static final int DIRECTION_RIGHT = 1;
+    public static final int DIRECTION_LEFT = -1;
+    public static final int DIRECTION_UP = 2;
+    public static final int DIRECTION_DOWN = -2;
+    private Snake snake;
+    private Board board;
+    private int direction;
+    private boolean gameOver;
+
+    public Game(Snake snake, Board board) {
+        this.snake = snake;
+        this.board = board;
+    }
+
+    public Snake getSnake() {
+        return this.snake;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public Board getBoard() {
+        return this.board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public boolean isGameOver() {
+        return this.gameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        this.gameOver = gameOver;
+    }
+
+    public int getDirection() {
+        return this.direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public void update() {
+        if (!gameOver) {
+            if (direction != DIRECTION_NONE) {
+                Cell nextCell = getNextCell(snake.getHead());
+
+                //check if crash and end game
+                if (snake.checkCrash(nextCell)) {
+                    setDirection(DIRECTION_NONE);
+                    setGameOver(true);
+                } else {
+                    snake.move(nextCell);
+                    // if player gets food, snake grows and make new food
+                    if (nextCell.getCellType() == CellType.FOOD) {
+                        snake.grow();
+                        board.generateFood();
+                    }
+                }
+            }
+        }
+    }
+
+    private Cell getNextCell(Cell currentPosition) {
+        return null;
+    }
 }
