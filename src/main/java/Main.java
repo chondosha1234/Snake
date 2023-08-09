555dc0a9a52b127c5dbbdb2e254409297ae575db
chondosha1234 <jmiller61193@gmail.com>
2023-08-09 00:45:04 -0400

    wrote basic main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        SnakeGui snakeGui = new SnakeGui();
+        snakeGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        snakeGui.setTitle("Snake Game");
+        snakeGui.pack();
+        snakeGui.setVisible(true);
+    }
 }
